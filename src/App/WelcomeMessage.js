e4dc60090e36bcb1b4a734aa322627c8ccbecaba
hyunsu joo <hs9880@gmail.com>
2019-03-16 00:23:48 -0400

    Add app.WelcomeMessage

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+ const WelcomeMessage =(props)=>{
+    return(
+      <div>
+        
+        <h1>Welcome to Crypto Tracking</h1>
+      </div>
+    )
+  }
+
+  export default WelcomeMessage;
